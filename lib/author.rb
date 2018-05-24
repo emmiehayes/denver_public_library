2ab88d69e2287bffa75e10e16b5f84bb46454ebc
Emmie Hayes <emmiehayes2@gmail.com>
2018-05-24 10:51:08 -0600

    author class and test

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,24 @@
+require './lib/book'
+require 'pry'
 class Author
 
+  attr_reader :author_data,
+              :books
+
+  def initialize(author_data)
+    @author_data = {
+                    first_name: author_data[:first_name],
+                    last_name: author_data[:last_name]
+                    }
+    @books = []
+  end
+
+  def add_book(title, publication_date)
+    book_data = {}
+    book_data[:author_first_name] = author_data[:first_name]
+    book_data[:author_last_name] = author_data[:last_name]
+    book_data[:title] = title
+    book_data[:publication_date] = publication_date
+    @books << Book.new(book_data)
+  end
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,66 @@
 require './test/test_helper'
-
+require './lib/author'
+require './lib/book'
+require 'pry'
 class AuthorTest < Minitest::Test
 
+  def test_it_exists
+    author = Author.new({first_name: "Charlotte",
+                         last_name: "Bronte"})
+    assert_instance_of Author, author
+  end
+
+  def test_it_has_attributes
+    author = Author.new({first_name: "Charlotte",
+                         last_name: "Bronte"})
+
+    assert_equal "Charlotte", author.author_data[:first_name]
+    assert_equal "Bronte", author.author_data[:last_name]
+  end
+
+  def test_it_can_add_a_book
+    author = Author.new({first_name: "Charlotte",
+                         last_name: "Bronte"})
+
+    book = Book.new({author_first_name: "Charlotte",
+                     author_last_name: "Bronte",
+                     title: "Jane Eyre",
+                     publication_date: "October 16, 1847"
+                    })
+    assert_equal book, author.add_book("Jane Eyre", "October 16, 1847")
+  end
+
+  def test_it_can_store_added_book
+    author = Author.new({first_name: "Charlotte",
+                         last_name: "Bronte"})
+
+    book = Book.new({author_first_name: "Charlotte",
+                     author_last_name: "Bronte"
+                    })
+    author.add_book("Jane Eyre", "October 16, 1847")
+    assert_equal 1, author.books.count
+  end
+
+  def test_it_can_add_a_new_book
+    author = Author.new({first_name: "Charlotte",
+                         last_name: "Bronte"})
+
+    book = Book.new({author_first_name: "Charlotte",
+                     author_last_name: "Bronte"
+                    })
+    assert_equal book, author.add_book("Villette", "1853")
+  end
+
+  def test_it_has_more_than_one_book_stored_in_books
+    author = Author.new({first_name: "Charlotte",
+                         last_name: "Bronte"})
+
+      book = Book.new({author_first_name: "Charlotte",
+                       author_last_name: "Bronte"
+                      })
+    author.add_book("Jane Eyre", "October 16, 1847")
+    author.add_book("Villette", "1853")
+    assert_equal 2, author.books.count
+  end
 
 end
